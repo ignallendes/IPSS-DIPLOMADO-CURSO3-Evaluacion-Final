53c5aa9a97a3b704dc4e309f6ea565fc53f21c8f
STAIN <igna.allendes@gmail.com>
2026-08-08 15:21:14 -0400

    feat: Se agrega string de conexión de closter y se genera JWT aleatorio

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ import mongoose from 'mongoose'
 //    apunta a una base local. Nadie debe poder entrar a tu base desde tu repo.
 
 const MONGODB_URI =
-  'mongodb+srv://usuario-mongo:clave-secreta@cluster0.xxxxx.mongodb.net/plataforma'
+  'mongodb+srv://ignaallendes_db_user:rdh2R4LNHvOhwrnU@cluster0.c5bjkyw.mongodb.net/plataforma'
 
 export const conectar = async () => {
   await mongoose.connect(MONGODB_URI)
--- config/jwt.js
+++ config/jwt.js
@@ -4,7 +4,7 @@
 // En un proyecto real esto va en una variable de entorno. Para la evaluación,
 // puedes dejarlo aquí — pero cámbialo por una cadena tuya, larga y aleatoria.
 
-export const JWT_SECRET = 'cambia-esto-por-un-secreto-largo-y-aleatorio'
+export const JWT_SECRET = 'a892340d5944a58884eb86061b6780b0a847ef4990f42bfc3675d8a2934d62dd023028de69ec244840996766618d8e720594c221f13cfe921acf01d688d27ad6'
 
 // Cuánto dura la sesión.
 export const JWT_EXPIRA = '7d'
